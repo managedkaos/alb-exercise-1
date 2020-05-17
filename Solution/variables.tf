0e3587a879f7bea4e2126dd159060cbafffd54bd
Michael <michael@managedkaos.com>
2020-05-16 20:52:55 -0700

    Update solution to use provider variables

--- Solution/variables.tf
+++ Solution/variables.tf
@@ -1,3 +1,15 @@
+variable "region" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
 variable "name" {
   type    = string
   default = "solution"
